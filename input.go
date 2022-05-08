00064a7992a4ffef0337870e97779aa564e25fad
talwat <talrac1020@gmail.com>
2022-05-08 17:08:08 +0200

    Release 0.34

--- input.go
+++ input.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-const version = "0.34-alpha"
+const version = "0.34"
 
 var purge, debug, assumeYes, force, noDeps, ignoreRoot bool = false, false, false, false, false, false
 
